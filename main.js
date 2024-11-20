74d446bc47bb5622c39e68c866ef3288facbd30d
AnaHanaku <anahanakuuchiha@gmail.com>
2024-11-20 05:12:42 -0300

    Subida inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+let cantidad = document.getElementById("cantidad");
+let boton = document.getElementById("generar");
+let contraseña = document.getElementById("contrasena");
+
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generar(){
+    let numeroDigitado = parseInt (cantidad.value);
+    
+    if( numeroDigitado < 8){
+    alert("La cantidad de caracteres tiene que se mayor que 8");   
+    }
+    
+    let password = "";
+    
+    for(let i=0; i < numeroDigitado; i++){
+        
+            let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+            
+        password+=caracterAleatorio;
+    }
+    
+    contraseña.value = password;
+    
+}
